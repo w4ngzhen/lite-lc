11bee5c29e00e2fa83af341e48eaaa5817066492
w4ngzhen <w4ngzhen@hotmail.com>
2022-12-01 11:26:26 +0800

    add-modify: 修改BuildEngine，引入ElementNodeResolveAspectHandler，并在innerBuild中进行切面处理。

--- packages/core/src/engine/BuildEngine.ts
+++ packages/core/src/engine/BuildEngine.ts
@@ -2,6 +2,7 @@ import {ElementNode} from "../meta/ElementNode";
 import {TypeBuilderManager} from "../builder/TypeBuilderManager";
 import {ReactNode} from "react"
 import {ComponentBuildAspectHandler} from "./aspect/ComponentBuildAspectHandler";
+import {ElementNodeResolveAspectHandler} from "./aspect/ElementNodeResolveAspectHandler";
 
 /**
  * 构建引擎
@@ -19,6 +20,15 @@ export class BuildEngine {
     }
 
     /**
+     引擎所持有的"元素节点解析切面处理"
+     @private
+     */
+    private _elementNodeResolveAspectHandler?: ElementNodeResolveAspectHandler;
+    set elementNodeResolveAspectHandler(value: ElementNodeResolveAspectHandler) {
+        this._elementNodeResolveAspectHandler = value;
+    }
+
+    /**
      * 构建：通过传入ElementNode信息，得到该节点对应供React渲染的ReactNode
      * @param rootEleNode
      */
@@ -32,11 +42,24 @@ export class BuildEngine {
      * @param rootPath
      */
     private innerBuild(originRootEleNode: ElementNode, rootPath: string): ReactNode | undefined {
-        if (!originRootEleNode) {
+
+        let resolvedRootEleNode: ElementNode;
+        if (this._elementNodeResolveAspectHandler) {
+            // BuildEngine使用者可以定义元素节点解析切面处理，实现定制化
+            console.debug('进入元素节点解析切面处理');
+            resolvedRootEleNode =
+                this._elementNodeResolveAspectHandler(originRootEleNode, {
+                    path: rootPath
+                })
+        } else {
+            resolvedRootEleNode = originRootEleNode;
+        }
+
+        if (!resolvedRootEleNode) {
             return undefined;
         }
 
-        const {type, children} = originRootEleNode;
+        const {type, children} = resolvedRootEleNode;
 
         // 如果有子元素，则递归调用自身，获取子元素处理后的ReactNode
         const childrenReactNode =
@@ -58,7 +81,7 @@ export class BuildEngine {
         const reactNode = typeBuilder.build(
             {
                 path: rootPath,
-                elementNode: originRootEleNode
+                elementNode: resolvedRootEleNode
             },
             childrenReactNode
         )
@@ -68,7 +91,7 @@ export class BuildEngine {
             console.debug('进入组件构建切面处理')
             return this._componentBuildAspectHandler(reactNode, {
                 path: rootPath,
-                elementNode: originRootEleNode
+                elementNode: resolvedRootEleNode
             })
         }
 

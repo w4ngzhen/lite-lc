baab6736a14c5ec21f6f1f0c7fd1a15f7267ed3c
w4ngzhen <w4ngzhen@hotmail.com>
2023-02-15 23:19:33 +0800

    modify: 修改DesignCanvas相关逻辑，实现ElementNodeDesignTreePanel组件与BuildEngine生成的组件对于选中的节点path，同步分别高亮树形节点和UI组件。

--- packages/core/src/canvas/design/DesignCanvas.tsx
+++ packages/core/src/canvas/design/DesignCanvas.tsx
@@ -1,6 +1,8 @@
 import React, {useMemo, useState} from "react";
 import {BuildEngine} from "../../engine/BuildEngine";
 import {ElementNodeDesignWrapper, ElementNodeDesignWrapperProps} from "./ElementNodeDesignWrapper";
+import {ElementNode} from "../../meta/ElementNode";
+import {ElementNodeDesignTreePanel} from "./ElementNodeDesignTreePanel";
 
 interface DesignCanvasProps {
     /**
@@ -15,7 +17,7 @@ export const DesignCanvas = (props: DesignCanvasProps) => {
         rootNodeSchemaJson
     } = props;
 
-    // 存储单机选中的path的state
+    // 譬如：selectedNodePath => "/page/panel/button"
     const [selectedNodePath, setSelectedNodePath] = useState<string>('');
 
     // 经过切面绑定的buildEngine
@@ -28,8 +30,7 @@ export const DesignCanvas = (props: DesignCanvasProps) => {
                 nodePath: path,
                 isSelected: path === selectedNodePath,
                 onClick: () => {
-                    console.debug('wrapper onClick')
-                    setSelectedNodePath(path)
+                    setSelectedNodePath(path);
                 }
             }
 
@@ -42,24 +43,51 @@ export const DesignCanvas = (props: DesignCanvasProps) => {
         return engine;
     }, [selectedNodePath]);
 
+    const elementNode = useMemo(() => {
+        return JSON.parse(rootNodeSchemaJson) as ElementNode;
+    }, [rootNodeSchemaJson]);
+
     // 经过buildEngine + schema 创建的React组件（已经考虑的基本的异常处理）
     const renderComponent = useMemo(() => {
         try {
-            const eleNode = JSON.parse(rootNodeSchemaJson);
-            return buildEngine.build(eleNode);
+            return buildEngine.build(elementNode);
         } catch (e) {
             return <div>构建出错：{e.message}</div>
         }
-    }, [rootNodeSchemaJson, selectedNodePath]);
+    }, [elementNode, selectedNodePath]);
 
     return (
         <div style={{
             width: '100%',
             height: '100%',
-            padding: '5px'
+            padding: '5px',
+            display: 'flex',
+            flexDirection: 'row'
         }}>
-            {renderComponent}
+            <div style={{
+                width: '250px',
+                height: '100%'
+            }}>
+                <ElementNodeDesignTreePanel
+                    rootElementNode={elementNode}
+                    selectedElementNodePath={selectedNodePath}
+                    onElementNodeSelected={(selectedPath) => {
+                        if (selectedPath === '') {
+                            // 代表再次点击节点，取消了选中
+                            console.debug('取消选中');
+                        }
+                        setSelectedNodePath(selectedPath);
+                    }}
+                />
+            </div>
+            <div style={{
+                width: 'calc(100% - 250px)',
+                height: '100%'
+            }}>
+                {renderComponent}
+            </div>
         </div>
     )
 
 }
+

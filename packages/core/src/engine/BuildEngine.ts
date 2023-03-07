073fb5e5ae3aa87e611183219506685690aafe60
w4ngzhen <w4ngzhen@hotmail.com>
2023-03-07 12:59:08 +0800

    modify: BuildEngine递归增加path标识组件唯一性，并作为key交给react创建ReactNode。

--- packages/core/src/engine/BuildEngine.ts
+++ packages/core/src/engine/BuildEngine.ts
@@ -9,14 +9,17 @@ export class BuildEngine {
      * @param componentNode
      */
     build(componentNode: ComponentNode) {
-        return this.innerBuild(componentNode);
+        // 起始节点，需要构造一个起始path传入innerBuild
+        // 根节点由于不属于某一个父级的子元素，所以不存在'@${index}'
+        return this.innerBuild(componentNode, '/' + componentNode.componentName);
     }
 
     /**
      * 构建：通过传入 ComponentNode 信息，得到该节点对应供React渲染的ReactNode
      * @param componentNode
+     * @param path
      */
-    private innerBuild(componentNode: ComponentNode) {
+    private innerBuild(componentNode: ComponentNode, path: string) {
 
         if (!componentNode) {
             return undefined;
@@ -26,8 +29,11 @@ export class BuildEngine {
 
         // 如果有子元素，则递归调用自身，获取子元素处理后的ReactNode
         const childrenReactNode =
-            (children || []).map((childNode) => {
-                return this.innerBuild(childNode);
+            (children || []).map((childNode, index) => {
+                // 子元素路径：
+                // 父级路径（也就是当前path）+ '/' + 子元素名称 + '@' + 子元素所在索引
+                const childPath = `${path}/${childNode.componentName}@${index}`;
+                return this.innerBuild(childNode, childPath);
             });
 
         // 通过 COMPONENT_MAP 来查找对应组件的构造器
@@ -35,7 +41,7 @@ export class BuildEngine {
 
         return React.createElement(
             componentConstructor,
-            {...props},
+            {...props, key: path},
             childrenReactNode.length > 0 ? childrenReactNode : undefined
         )
     }
